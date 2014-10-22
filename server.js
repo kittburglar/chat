c49bc645fbacab945ca3a0af64f235d819b50c25
Kittiphong Xayasane <kxayasan@gmail.com>
2014-10-22 15:00:59 -0400

    added data into mongodb coll

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-console.log("Worked!");
+var mongo = require('mongodb').MongoClient,
+	client = require('socket.io').listen(8080).sockets;
+
+mongo.connect('mongodb://127.0.0.1/chat', function(err, db){
+	if(err) throw err;
+
+	client.on('connection', function(socket){
+		
+		var col = db.collection('messages');
+
+		// Wait for input
+		socket.on('input', function(data){
+			var name = data.name,
+				message = data.message
+				whitespacePattern = /^\s*$/;
+
+			if(whitespacePattern.test(name) || whitespacePattern.test(message)) {
+				console.log('Invalid');
+			} else{
+				col.insert({name: name, message: message}, function(){
+				console.log("Inserted");
+				});
+			}
+
+			
+
+		});
+
+	});
+});
+
