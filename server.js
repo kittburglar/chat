a588ee7c57aa72b269494c44b1a2aa192c35e93a
Kittiphong Xayasane <kxayasan@gmail.com>
2014-10-23 11:47:35 -0400

    outputted statuses

--- server.js
+++ server.js
@@ -6,19 +6,25 @@ mongo.connect('mongodb://127.0.0.1/chat', function(err, db){
 
 	client.on('connection', function(socket){
 		
-		var col = db.collection('messages');
+		var col = db.collection('messages'),
+			sendStatus = function(s){
+				socket.emit('status', s);
+			};
 
 		// Wait for input
 		socket.on('input', function(data){
 			var name = data.name,
-				message = data.message
+				message = data.message,
 				whitespacePattern = /^\s*$/;
 
 			if(whitespacePattern.test(name) || whitespacePattern.test(message)) {
-				console.log('Invalid');
+				sendStatus('Name and message is required.');
 			} else{
 				col.insert({name: name, message: message}, function(){
-				console.log("Inserted");
+					sendStatus({
+						message: "Message Sent",
+						clear: true
+					});
 				});
 			}
 
