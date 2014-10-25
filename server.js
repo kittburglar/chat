397e7517a3e1746bee0be0a7cb15570a5f7f4bd8
Kittiphong Xayasane <kxayasan@gmail.com>
2014-10-25 15:49:58 -0400

    outputted chat messages

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ mongo.connect('mongodb://127.0.0.1/chat', function(err, db){
 			sendStatus = function(s){
 				socket.emit('status', s);
 			};
+ 
+		//Emit all messages
+		col.find().limit(100).sort({_id: 1}).toArray(function(err, res) {
+			if(err) throw err;
+			socket.emit('output', res);
+		});
 
 		// Wait for input
 		socket.on('input', function(data){
@@ -21,6 +27,10 @@ mongo.connect('mongodb://127.0.0.1/chat', function(err, db){
 				sendStatus('Name and message is required.');
 			} else{
 				col.insert({name: name, message: message}, function(){
+
+					// Emit latest message to ALL clients
+					client.emit('output', [data]);
+
 					sendStatus({
 						message: "Message Sent",
 						clear: true
